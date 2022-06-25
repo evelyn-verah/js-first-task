27c20c87848e7427167b64086c675a87ee28c677
evelyn-verah <evelynnzilani15@gmail.com>
2022-06-25 15:18:02 +0200

    First Javascript Task

---
+++ life.js
@@ -0,0 +1,7 @@
+//  let name = window.prompt("Whats your name?");
+ alert(" My name is Evelyn Nzilani. ");
+
+//  let height = window.prompt("What's your height?")
+ alert(" My height is  177 cm. ");
+//  let country = window.prompt("Which Country are you from?");
+ alert( " I am from Kenya.");
